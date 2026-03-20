9f2fdcc21755544ee23f0fe783d54bb9ed308727
Makoto Takita <makototakita@yahoo.co.jp>
2026-03-20 23:16:16 +0900

    delete unnecessary files

--- chap-02/args/target/debug/args.d
+++
@@ -1,1 +0,0 @@
-/home/mtakita/GitHub/Linux/programming-rust/chap-02/args/target/debug/args: /home/mtakita/GitHub/Linux/programming-rust/chap-02/args/src/main.rs
--- chap-02/args/target/debug/deps/args-476d0da84052d673.d
+++
@@ -1,5 +0,0 @@
-/home/mtakita/GitHub/Linux/programming-rust/chap-02/args/target/debug/deps/args-476d0da84052d673.d: src/main.rs
-
-/home/mtakita/GitHub/Linux/programming-rust/chap-02/args/target/debug/deps/args-476d0da84052d673: src/main.rs
-
-src/main.rs:
